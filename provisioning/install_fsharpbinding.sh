adae84b789702e7ec5254b095329005ccc9efd6d
Fraser Waters <fraser.waters@arm.com>
2014-04-14 15:59:04 +0100

    Patch to F# 3.1

--- provisioning/install_fsharpbinding.sh
+++ provisioning/install_fsharpbinding.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Download, build and install mono develop and fsharp
-git clone https://github.com/fsharp/fsharpbinding.git
-cd fsharpbinding/monodevelop
+git clone -b 3.2.28.1 https://github.com/fsharp/fsharpbinding.git
+cd fsharpbinding
+git apply /vagrant/provisioning/fsharpbinding.patch
+cd monodevelop
 ./configure.sh --prefix=/usr
 make
 make install
