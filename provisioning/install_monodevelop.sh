5b0075d4861c9b0066a54f7e3a01089345dafb16
Fraser Waters <frassle@gmail.com>
2014-06-03 11:30:53 +0100

    Use MonoDevelop 5.0, no longer need fsharpbinding.

--- provisioning/install_monodevelop.sh
+++ provisioning/install_monodevelop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download, build and install mono develop and fsharp
-git clone --recursive -b monodevelop-4.2.2 https://github.com/mono/monodevelop.git
+git clone --recursive -b monodevelop-5.0 https://github.com/mono/monodevelop.git
 cd monodevelop
 ./configure --prefix=/usr --profile=stable
 make
